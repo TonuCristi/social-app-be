ebc1539644821886558f25818079e3a450acaa4c
Tonu Cristian <chrisdev2002@gmail.com>
2024-05-02 00:33:36 +0300

    working on post routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,6 +5,9 @@ export const createPost = async (req, res) => {
   try {
     const post = req.body;
 
+    if (!post.description && !post.avatar)
+      throw new Error("Complete at least one field!");
+
     await Post.create(post);
 
     res.status(200).json({ message: "Post added!" });
@@ -40,6 +43,50 @@ export const getPosts = async (req, res) => {
     if (!posts) throw new Error("Posts not found!");
 
     res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Delete post
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Post.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "Post deleted!" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Update post description
+export const updatePostDescription = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = req.body;
+
+    await Post.findByIdAndUpdate(id, { description: post.description });
+
+    res.status(200).json({ message: "Post description updated!" });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Update post avatar
+export const updatePostAvatar = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = req.body;
+
+    await Post.findByIdAndUpdate(id, { image: post.avatar });
+
+    res.status(200).json({ message: "Post avatar updated!" });
   } catch (err) {
     console.log(err);
     res.status(400).json({ error: err.message });
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,11 @@ import express from "express";
 import { authRequest } from "../middleware/authRequest.js";
 import {
   createPost,
+  deletePost,
   getPost,
   getPosts,
+  updatePostAvatar,
+  updatePostDescription,
 } from "../controllers/postController.js";
 
 const router = express.Router();
@@ -19,5 +22,14 @@ router.get("/post/:id", getPost);
 
 // Get all posts
 router.get("/:userId", getPosts);
+
+// Delete post
+router.delete("/post/deletePost/:id", deletePost);
+
+// Update post description
+router.put("/post/updateDescription/:id", updatePostDescription);
+
+// Update post avatar
+router.put("/post/updateAvatar/:id", updatePostAvatar);
 
 export default router;
